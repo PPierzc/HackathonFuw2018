4c72d6fe0f033d4acbef3ee512c8e19123a5b411
Pierzchlewicz <ppierzc@gmail.com>
2018-03-24 22:26:01 +0100

    filtering

---
+++ Gory_Doliny/filters.py
@@ -0,0 +1,14 @@
+import numpy as np
+import scipy.ndimage.filters as filters
+
+def filter_map(data):
+    kernel = np.array([
+        [0, 0, 1, 0, 0],
+        [0, 1, 1, 1, 0],
+        [1, 1, 2, 1, 1],
+        [0, 1, 1, 1, 0],
+        [0, 0, 1, 0, 0]
+    ])
+    kernel = kernel/14
+    filtered_data = filters.convolve(data, kernel)
+    return filtered_data
